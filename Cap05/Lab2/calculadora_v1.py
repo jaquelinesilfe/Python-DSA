0beca289c48791da0364f9e68374cac0e253def9
Jaqueline Silfe <jaquelinefernandes6@gmail.com>
2023-09-23 12:03:52 -0300

    modulos introdutorios

--- Cap05/Lab2/calculadora_v1.py
+++ Cap05/Lab2/calculadora_v1.py
@@ -1,8 +1,37 @@
 # Calculadora em Python
 
-# Desenvolva uma calculadora em Python com tudo que você aprendeu nos capítulos até aqui no curso. 
-# A solução será apresentada no próximo capítulo!
+import math
 
 print("\n******************* Calculadora em Python *******************")
+
+print ("Selecione o numero da operacao desejada: ")
+
+print(" 1 - Soma")
+print(" 2 - Subtracao")
+print(" 3 - Multiplicacao")
+print(" 4 - Divisao")
+operacao = input("Digite sua operacao (1/2/3/4): ")
+num1 = input("Digite o primeiro numero: ")
+num2 = input("Digite o segundo numero: ")
+
+
+
+if operacao == "1" :
+    resultado = int(num1) + int(num2)
+    print("Resultado: " + str(resultado))
+if operacao == "2" :
+    resultado = int(num1) - int(num2)
+    print("Resultado: " + str(resultado))
+if operacao == "3" :
+    resultado = int(num1) * int(num2)
+    print("Resultado: " + str(resultado))
+if operacao == "4" :
+    resultado = int(num1) / int(num2)
+    print("Resultado: "  + str(resultado))
+else : 
+    print("Selecione uma opcao valida")
+    
+        
+
 
 
